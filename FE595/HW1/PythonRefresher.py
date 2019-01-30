6a4424f49944d196cde5218dd88c7c82c992731c
william0423118 <dwang35@stevens.edu>
2019-01-30 12:42:55 -0500

    Add files via upload
    change to one period.

--- FE595/HW1/PythonRefresher.py
+++ FE595/HW1/PythonRefresher.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(-2 * np.pi, 2 * np.pi, 201) # get the x- axis horizon
+x = np.linspace(0, 2 * np.pi, 201) # get the x- axis horizon
 plt.plot(x, np.sin(x), label='sine')  #plot sine
 plt.plot(x, np.cos(x), label='cosine') #plot cosine
 plt.legend(loc='best') # put the legend in the suitable place
